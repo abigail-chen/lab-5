a2c5a77c5bb17fa7a39247d09b75e3950687ae61
Qifan Yang <qifanfan.yang@mail.utoronto.ca>
2024-10-21 11:13:56 -0400

    use_case.logout

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -103,6 +103,7 @@ public class LoggedInView extends JPanel implements PropertyChangeListener {
                         // TODO: execute the logout use case through the Controller
                         // 1. get the state out of the loggedInViewModel. It contains the username.
                         // 2. Execute the logout Controller.
+
                     }
                 }
         );
